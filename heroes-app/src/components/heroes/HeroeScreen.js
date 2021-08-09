4c26d6b717a983352d48e6a9d84df1269165f493
Xiomara <xiomaravictoriaocampo@gmail.com>
2021-08-09 10:34:28 -0300

    creating the first Route

---
+++ heroes-app/src/components/heroes/HeroeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroeScreen = () => {
+    return (
+        <div>
+            <h1>HeroeScreen</h1>
+        </div>
+    )
+}
---
+++ heroes-app/src/components/login/LoginScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LoginScreen = () => {
+    return (
+        <div>
+            <h1>LOgin Screen</h1>
+        </div>
+    )
+}
