ad444a1fb95f7b43d323d31fce611c9706b0fd7e
Xiomara <xiomaravictoriaocampo@gmail.com>
2021-08-19 21:10:27 -0300

    filter

--- heroes-app/src/components/search/SearchScreen.js
+++ heroes-app/src/components/search/SearchScreen.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { heroes } from '../../data/heroes'
+import React, { useMemo } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard'
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
-export const SearchScreen = () => {
+export const SearchScreen = ({history}) => {
 
-    const heroesFiltered=heroes;
+    const location=useLocation();
+
+    const{q=''}=queryString.parse(location.search);
+    console.log(q);
+
     
-
     const [formValues,handleInputChange]=useForm({
-        searchText:''
+        searchText:q
     });
 
     const {searchText}=formValues;
 
+    const heroesFiltered=useMemo(()=>getHeroesByName(q),[q])
+
+  
+    
+
+
     const handleSearch=(e)=>{
         e.preventDefault();
-        console.log(searchText)
+        history.push(`?q=${searchText}`);
 
      }
 
@@ -60,6 +72,22 @@ export const SearchScreen = () => {
                         <h4>Result</h4>
                         <hr/>
 
+                        {
+                            (q==='')
+                                &&
+
+                                <div className='alert alert-info'>
+                                    Search a hero
+                                </div>
+                        }
+                         {
+                            (q!==''&& heroesFiltered.length===0)
+                                &&
+
+                                <div className='alert alert-danger'>
+                                    There is no a hero with{q}
+                                </div>
+                        }
                         {
                             heroesFiltered.map(hero=>(
                                 <HeroCard
